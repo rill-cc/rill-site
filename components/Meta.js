e0263fac62bbaaba10b7334e8dc22a64c0428fa2
Maksim Karalevich <m@karalevich.com>
2020-04-01 00:28:24 -0700

    update meta

--- components/Meta.js
+++ components/Meta.js
@@ -7,17 +7,17 @@ export default function Meta() {
 			<meta charSet='utf-8' />
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 		  <meta name="Rill—Common Connections" />
-	  	<meta name="description" content="Open platform that helps interconnect environmental events and track updates." />
-			<meta name="keywords" content="education, unschooling, free-schooling, school" />
+	  	<meta name="description" content="An open platform that helps interconnect environmental events and track updates." />
+			<meta name="keywords" content="science, research, education, network" />
 		  
-			<meta property="og:image" content="https://rill.logo.jpg" />
+			<meta property="og:image" content="https://rill.cc/images/thumb.jpg" />
 			<meta property="image:width" content="1920" />
 			<meta property="image:height" content="1080" />
 			<meta property="og:title" content="Rill—Common Connections" />
-		  <meta property="og:description" content="Open platform that helps interconnect environmental events and track updates." />
+		  <meta property="og:description" content="An open platform that helps interconnect environmental events and track updates." />
 			<meta property="og:url" content="https://rill.cc" />
 			<meta property="og:type" content="website" />
-		  <link rel="icon" href="/fav.png" />
+		  <link rel="icon" href="/images/fav.jpg" />
 		  <title>Rill—Common Connections</title>
 		</Head>
 	)
